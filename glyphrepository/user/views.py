c253685d0054872f657261c8b2d2bf7c0d498b1f
James Scott-Brown <james@jamesscottbrown.com>
2017-08-08 16:59:46 -0400

    Add a per-user table of glyphs

--- glyphrepository/user/views.py
+++ glyphrepository/user/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
+
+from glyphrepository.user.models import User
+from glyphrepository.glyph.models import Glyph
 
 blueprint = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
@@ -9,5 +12,15 @@ blueprint = Blueprint('user', __name__, url_prefix='/users', static_folder='../s
 @blueprint.route('/')
 @login_required
 def members():
-    """List members."""
-    return render_template('users/members.html')
+    """If no user specified, redirect to homepage."""
+    return redirect(url_for('public.home'))
+
+
+@blueprint.route('/<user_id>')
+def user(user_id):
+    """Show all Glyphs by a single user."""
+
+    user = User.query.filter(User.id == user_id).all()[0]
+    glyphs = Glyph.query.filter(Glyph.user_id == user_id).all()
+
+    return render_template('user/user.html', user=user, glyphs=glyphs)
