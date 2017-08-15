d5cd53b4c198e535537a09bd3f4201d7a73ba8d5
James Scott-Brown <james@jamesscottbrown.com>
2017-08-15 15:03:58 +0100

    Fix table filtering after 'SBOL Status' column added

--- glyphrepository/static/libs/search_glyph_table.js
+++ glyphrepository/static/libs/search_glyph_table.js
@@ -9,7 +9,7 @@
 
 	  // Loop through all table rows, and hide those who don't match the search query
 	  for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[2];
+	    td = tr[i].getElementsByTagName("td")[3];
 	    if (td) {
 	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 	        tr[i].style.display = "";
